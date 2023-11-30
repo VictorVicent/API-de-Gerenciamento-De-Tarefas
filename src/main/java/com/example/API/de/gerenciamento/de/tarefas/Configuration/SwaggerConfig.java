c5592adaeafe373d8f4996ee89f7a33a0543e203
Victor Vicente <116645036+VictorVicent@users.noreply.github.com>
2023-11-30 20:54:27 -0300

    Update SwaggerConfig.java

--- src/main/java/com/example/API/de/gerenciamento/de/tarefas/Configuration/SwaggerConfig.java
+++ src/main/java/com/example/API/de/gerenciamento/de/tarefas/Configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.API.de.gerenciamento.de.tarefas.configuration;
+package com.example.API.de.gerenciamento.de.tarefas.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +16,9 @@ public class SwaggerConfig {
          .description("Esta API é utilizada para agendamento de tarefas\n\n" +
          "Contatos:\n\n" +
          "André Luiz F. Martins - andre.martins30@fatec.sp.gov.br\n\n" +
-         "Diego Sampaio Gabarrão - diego.gabarrao@fatec.sp.gov.br\n")+
-         "Pedro Estevam Blanco Campbell - pedro.campbell@fatec.sp.gov.br\n")+
+         "Diego Sampaio Gabarrão - diego.gabarrao@fatec.sp.gov.br\n"+
+         "Pedro Estevam Blanco Campbell - pedro.campbell@fatec.sp.gov.br\n"+
          "Victor Vicente Alves - victor.alves26@fatec.sp.gov.br\n")
          .version("v0.0.1")
          .license(new License()
            .name("Apache 2.0").url("http://springdoc.org")));
-   }
-}
